09d4c1335aacc660c40a7498f274854f52d28bf6
Dmitry Kolosov <mitrichius@gmail.com>
2020-08-14 20:23:37 +0300

    Symlink favicon on install

--- remote-install.sh
+++ remote-install.sh
@@ -4,4 +4,5 @@ git init
 git submodule add https://github.com/mitrichius/WishlistJS.git src
 cp -R src/exampleSite/. ./
 ln -s src/index.html index.html
+ln -s src/favicon.png favicon.png
 git rm --cached src/
